7332572ea8e3aacbdc9d56287f67c3818b3205e8
PAUL21\paul6 <paul6101301@gmail.com>
2024-10-13 23:19:08 +0900

    온보딩 페이지, 바텀 네비게이션 구현

---
+++ TBG2/app/src/main/java/com/example/tbg/HomeActivity.java
@@ -0,0 +1,51 @@
+package com.example.tbg;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.tbg.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.assignment) {
+                replaceFragment(new AssignmentFragment());
+            } else if (itemId == R.id.profile) {
+                replaceFragment(new Accountfragment());
+            }
+
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
+    }
+}
---
+++ TBG2/app/src/main/java/com/example/tbg/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.tbg;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private OnboardingAdapter onboardingAdapter;
+    private LinearLayout layoutOnboardingIndicators;
+    private MaterialButton buttonOnboardingAction;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layoutOnboardingIndicators = findViewById(R.id.layoutOnboardingIndicators);
+        buttonOnboardingAction = findViewById(R.id.buttonOnboardingAction);
+
+        setupOnboardingItems();
+
+        ViewPager2 onboardingViewPager = findViewById(R.id.onboardingViewPager);
+        onboardingViewPager.setAdapter(onboardingAdapter);
+
+        setupOnboardingIndicators();
+        setCurrentOnboardingIndicator(0);
+
+        onboardingViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentOnboardingIndicator(position);
+            }
+        });
+
+        buttonOnboardingAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (onboardingViewPager.getCurrentItem() + 1 < onboardingAdapter.getItemCount()) {
+                    onboardingViewPager.setCurrentItem(onboardingViewPager.getCurrentItem() + 1); // 다음 페이지로 이동
+                } else {
+                    // 마지막 페이지에 도달한 경우
+                    startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                    finish(); // OnboardingActivity 종료
+                }
+            }
+        });
+    }
+
+    private void setupOnboardingItems(){
+
+        List<OnboardingItem> onboardingItems = new ArrayList<>();
+
+        OnboardingItem itemPayOnline = new OnboardingItem();
+        itemPayOnline.setTitle("여행지 추천!");
+        itemPayOnline.setDescription("가고 싶은 곳이 \n있으신가요?");
+        itemPayOnline.setImage(R.drawable.onboarding1);
+
+        OnboardingItem itemOnTheWay = new OnboardingItem();
+        itemOnTheWay.setTitle("교통수단 예약 가능!");
+        itemOnTheWay.setDescription("각종 교통수단과 \n함께 할 수 있어요");
+        itemOnTheWay.setImage(R.drawable.onboarding2);
+
+        OnboardingItem itemEatTogether = new OnboardingItem();
+        itemEatTogether.setTitle("손쉽게 시작하는 여행!");
+        itemEatTogether.setDescription("타볼까와\n여행을 시작해봐요!");
+        itemEatTogether.setImage(R.drawable.onboarding3);
+
+        onboardingItems.add(itemPayOnline);
+        onboardingItems.add(itemOnTheWay);
+        onboardingItems.add(itemEatTogether);
+
+        onboardingAdapter = new OnboardingAdapter(onboardingItems);
+
+
+    }
+    private void setupOnboardingIndicators() {
+        ImageView[] indicators = new ImageView[onboardingAdapter.getItemCount()];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(20, 0, 20, 0);
+        for (int i = 0; i< indicators.length; i++){
+            indicators[i] = new ImageView(getApplication());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(
+                    getApplicationContext(),
+                    R.drawable.onboarding_indicator_inactive
+            ));
+            indicators[i].setLayoutParams(layoutParams);
+            layoutOnboardingIndicators.addView(indicators[i]);
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void setCurrentOnboardingIndicator(int index){
+        int childCount = layoutOnboardingIndicators.getChildCount();
+        for(int i = 0; i < childCount; i++) {
+            ImageView imageView = (ImageView) layoutOnboardingIndicators.getChildAt(i);
+            if(i == index){
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.onboarding_indicator_active)
+                );
+            } else{
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.onboarding_indicator_inactive)
+                );
+            }
+        }
+        if(index == onboardingAdapter.getItemCount() - 1) {
+            buttonOnboardingAction.setText("시작하기");
+        } else {
+            buttonOnboardingAction.setText("다음");
+        }
+    }
+}
