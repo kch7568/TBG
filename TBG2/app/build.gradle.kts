c9cdbda1ad780b48ef0b6f5b2729083384c71c97
PAUL21\paul6 <paul6101301@gmail.com>
2024-11-14 04:35:26 +0900

    HomeFragment api관련 코드 추가, api관리 클래스 추가, 유저 권한 설정 Manifest수정, 의존성 추가, homefragment xml 디자인 수정, strings 수정

--- TBG2/app/build.gradle.kts
+++ TBG2/app/build.gradle.kts
@@ -45,4 +45,12 @@ dependencies {
     androidTestImplementation (libs.espresso.core)
     implementation ("androidx.viewpager2:viewpager2:1.1.0")
     implementation ("com.google.android.material:material:1.12.0")
+    implementation ("androidx.viewpager2:viewpager2:1.0.0")
+    implementation ("com.google.android.gms:play-services-ads:23.5.0") //AD몹 (광고 배너 표시)
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0") //API 호출을 위한 Http 클라이언트
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.picasso:picasso:2.71828") //이미지 로드 라이브러리
+    implementation("com.google.android.gms:play-services-location:21.3.0") //gps 기반 위치 정보
+
+
 }
---
+++ TBG2/app/src/main/java/com/example/tbg/GeocodingResponse.java
@@ -0,0 +1,15 @@
+package com.example.tbg;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+public class GeocodingResponse {
+
+    @SerializedName("results")
+    public List<Result> results;
+
+    public class Result {
+        @SerializedName("formatted_address")
+        public String formattedAddress;
+    }
+}
